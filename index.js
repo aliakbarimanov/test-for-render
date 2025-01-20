9b3ccbb455bcf7e1bf651c130f3a22d2f71d4c05
aliakbarimanov <aliakbar.imanov@gmail.com>
2025-01-20 17:53:30 +0400

    Connected with MongoDB

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ app.get("/", (req, res, next) => {
 });
 
 app.listen(3000);
+
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect("mongodb+srv://aliakbarimanov:salam123@fastjob.a7bgf.mongodb.net/?retryWrites=true&w=majority&appName=fastjob")
+            .then(() => console.log("DB connected!"))
+            .catch(() => console.log("Connection failed!"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB();
